ba71c7ccbaa277be427458b8762907bd8df2eba9
dmadindividual <ayomidefalade@gmail.com>
2024-04-28 01:17:30 +0100

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,119 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import React, { useState, useEffect } from 'react';
+const { ethers, JsonRpcProvider } = require('ethers');
+import creator from '../app/ape1.png';
+import logo from '../app/eth.png';
+import Image from 'next/image';
+import './styles.css'
 
-export default function Home() {
+const Page = () => {
+  const [account, setAccount] = useState('');
+  const [connect, setConnect] = useState(false);
+  const [balance, setBalance] = useState('');
+
+  const failMessage = 'Please install MetaMask';
+  const successMessage = 'You have connected successfully to MetaMask';
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) {
+      alert(failMessage);
+      return;
+    }
+
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+      if (accounts.length > 0) {
+        const account = accounts[0];
+        setAccount(account);
+
+        const provider = new JsonRpcProvider('https://mainnet.infura.io/v3/0e5d95bf0bcc42de974ff8f63de693b8');
+        const balance = await provider.getBalance(account);
+        setBalance(balance.toString()); // Convert balance to string for display
+      } else {
+        alert(failMessage);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const connectWallet = async () => {
+    if(!window.ethereum)return
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts'
+    });
+
+    const account = accounts[0];
+    setAccount(account)
+    window.location.reload()
+  }
+
+  useEffect(() => {
+   
+
+    checkIfWalletIsConnected();
+  }); 
+
+  useEffect(()=>{
+    async function accountChanged(){
+      window.ethereum.on('accountsChanged', async function(){
+        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+
+        if(accounts.length ){
+          setAccount(accounts[0])
+        }else{
+          window.location.reload()
+        }
+    
+      })
+    }
+    accountChanged()
+  }, [])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+    <div className='card_container'>
+      {!account ? '': <span className='pro'>PRO</span>}
+      <Image src={creator} alt='avatar' width={80} height={80}/>
+      <h1>Check Ether</h1>
+
+      {!account ?(
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <div  className='message'>
+            <p>{failMessage}</p>
+
+          </div>
+          <Image src={logo} alt='logo' width={100} height={100}/>
+          <p>Welcome to ether account balance checker</p>
         </div>
-      </div>
+      ):(
+        <div>
+        <h6>Verified</h6>
+        <p>Ether Account and Balance Checker <br/> fin account details</p>
+        <div className='buttons'>
+          <button className='primary ghost' onClick={()=>{}}>Ether Account details</button>
+        </div>
+        </div>
+      )
+      }
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {!account && !connect ? (
+        <div className='buttons'>
+          <button className='primary' onClick={connectWallet}>Connect Wallet</button>
+        </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      ):(
+        <div className='skills'>
+ <h6>Your Ether</h6>
+  <ul>
+    <li className="meta_address">Account</li>
+    <li className="meta_address">{account}</li>
+    <li>Balance</li>
+    <li>{balance}</li>
+  </ul>
+        </div>
+      )}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Page;
